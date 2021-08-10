341adf38bf723fcff2875b800d46d177bc70db9b
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-10 11:41:31 -0400

    No cljs for now

--- src/main/com/yetanalytics/squuid.clj
+++ src/main/com/yetanalytics/squuid.clj
@@ -1,7 +1,7 @@
 (ns com.yetanalytics.squuid
-  #?@(:clj [(:require [com.yetanalytics.squuid.clj.uuid :as u]
-                      [com.yetanalytics.squuid.clj.time :as t])
-            (:import [java.time Instant])]))
+  (:require [com.yetanalytics.squuid.clj.uuid :as u]
+            [com.yetanalytics.squuid.clj.time :as t])
+  (:import [java.time Instant]))
 
 ;; This library generates sequential UUIDs, or SQUUIDs, based on the draft RFC
 ;; for v8 UUIDS:
