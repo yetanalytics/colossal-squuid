a8a3fc1b315bee68ae7528b8851dc1e3fd00ac4b
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-13 11:21:59 -0400

    Add new versions of functions with times

--- src/main/com/yetanalytics/squuid/uuid.cljs
+++ src/main/com/yetanalytics/squuid/uuid.cljs
@@ -1,6 +1,10 @@
 (ns com.yetanalytics.squuid.uuid
-  (:require [clojure.string :refer [join]]
+  (:require [clojure.string :as cstr :refer [join]]
             [com.yetanalytics.squuid.time :as t]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def ^:private hex-bitsize (js/BigInt 4))
 (def ^:private hex-bitmask (js/BigInt 0xF))
@@ -40,10 +44,19 @@
 
 (defn- rand-uuid-raw
   []
-  (-> (repeatedly 32 #(rand-int 16))
-      vec
-      (assoc 12 4)
-      (update 0 #(-> % (bit-clear 2) (bit-set 3)))))
+  (apply array
+         (-> (repeatedly 32 #(rand-int 16))
+             vec
+             (assoc 12 4)
+             (update 0 #(-> % (bit-clear 2) (bit-set 3))))))
+
+(defn- rand-uuid
+  []
+  (random-uuid))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- inc-uuid*
   [u]
@@ -67,6 +80,42 @@
       :else
       nil)))
 
+(defn- inc-uuid-2
+  [u]
+  (let [u-char-arr (js/Array.from (.toString u))
+        inc-char   (fn [c] (.toString (inc (js/parseInt c 16)) 16))
+        ret-uuid   (fn [char-arr] (UUID. (join char-arr) nil))]
+    (loop [i 35]
+      (cond
+        ;; Regular hexes: 0-F
+        (or (< 23 i)
+            (< 19 i 23)
+            (< 14 i 18))
+        (let [c (aget u-char-arr i)]
+          (if (or (identical? "F" c) (identical? "f" c))
+            (do (aset u-char-arr i "0")
+                (recur (dec i)))
+            (do (aset u-char-arr i (inc-char c))
+                (ret-uuid u-char-arr))))
+
+        ;; Variant hexes: 0-B
+        (= 17 i)
+        (let [c (aget u-char-arr i)]
+          (if (or (identical? "B" c) (identical? "b" c))
+            (do (aset u-char-arr i "0")
+                (recur (dec i)))
+            (do (aset u-char-arr i (inc-char c))
+                (ret-uuid u-char-arr))))
+
+        ;; Dashes: ignore
+        (#{18 23} i)
+        (recur (dec i))
+
+        :else
+        (throw (ex-info (str "Cannot increment UUID " u " any further.")
+                        {:type ::exceeded-max-uuid-node
+                         :uuid u}))))))
+
 (defn- make-squuid*
   [ts u]
   (let [ts'       (js/BigInt (t/time->millis ts))
@@ -75,6 +124,35 @@
                           '(8)
                           (drop 13 msb))]
     (concat msb' lsb)))
+
+(def ^:private bit-mask-16 (js/BigInt 0xFFFF))
+(def ^:private bit-shift-16 (js/BigInt 16))
+
+(defn- make-squuid-2
+  ([ts]
+   (make-squuid-2 ts (rand-uuid)))
+  ([ts u]
+   (let [make-padding
+         (fn [max-len s] (join (repeat (- max-len (count s)) "0")))
+        ;; Timestamp manips
+         ts'   (js/BigInt (inst-ms ts))
+         ts-hi (bit-shift-right ts' bit-shift-16)
+         ts-lo (bit-and ts' bit-mask-16)
+         ts-hs (.toString ts-hi 16)
+         ts-ls (.toString ts-lo 16)
+         ts-hp (make-padding 8 ts-hs)
+         ts-lp (make-padding 4 ts-ls)
+        ;; Base UUID manips
+         u-str  (subs (.toString u) 15)
+        ;; Cook some SQUUID
+         raw-squuid    (str ts-hp ts-hs
+                            "-"
+                            ts-lp ts-ls
+                            "-"
+                            "8" u-str)
+         cooked-squuid (UUID. raw-squuid nil)]
+     {:base-uuid u
+      :squuid    cooked-squuid})))
 
 (defn zero-uuid
   []
@@ -121,3 +199,30 @@
     (< u1 u2) -1
     (= u1 u2) 0
     (> u1 u2) 1))
+
+(comment
+
+  (def ct (t/current-time))
+  (def rv4 (rand-uuid))
+
+  ;; ~40 ms
+  (time
+   (dotimes [_ 1000]
+     (inc-uuid rv4)))
+  
+  ;; ~7 ms
+  (time
+   (dotimes [_ 1000]
+     (inc-uuid-2 rv4)))
+  
+  ;; ~50 ms
+  (time
+   (dotimes [_ 1000]
+     (:squuid (make-squuid ct rv4))))
+
+  ;; ~2.5 ms
+  (time
+   (dotimes [_ 1000]
+     (:squuid (make-squuid-2 ct rv4))))
+
+  )
